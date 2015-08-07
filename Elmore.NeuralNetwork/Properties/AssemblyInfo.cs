310c8e4a717d922be4114858b22dc69142a27a09
Tom Elmore <tom.elmore@easyroommate.com>
2015-08-07 14:40:00 +0100

    added nuget package definition

--- Elmore.NeuralNetwork/Properties/AssemblyInfo.cs
+++ Elmore.NeuralNetwork/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Elmore.NeuralNetwork")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A toy NN lib")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elmore")]
 [assembly: AssemblyProduct("Elmore.NeuralNetwork")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
